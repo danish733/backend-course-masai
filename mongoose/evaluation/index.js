3839519eb40ac2009704c0c19d44400cd0ecd2db
danish <danishreza733@gmail.com>
2024-08-04 16:26:22 +0530

    commit-3

--- mongoose/evaluation/index.js
+++ mongoose/evaluation/index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.use("/user", userRouter)
-app.use("/course", courseRouter)
+app.use( courseRouter)
 
 app.get("/",(req,res)=>{
     res.send("welcome to evalution")
--- mongoose/evaluation/routes/course.route.js
+++ mongoose/evaluation/routes/course.route.js
@@ -1,9 +1,47 @@
 const express = require("express")
+const CourseModel = require("../models/course.model")
 
 const courseRouter = express.Router()
 
-courseRouter.get("/",async(req,res)=>{
-    res.send("this is course router")
+courseRouter.get("/courses",async(req,res)=>{
+    try {
+        const {category, difficulty, q, page = 1, limit = 10} = req.query
+
+        const filter = {}
+        if(category) filter.category = category
+        if(difficulty) filter.difficulty = difficulty
+
+        const getCourse = await CourseModel.find(filter)
+            .skip((page-1)*limit)
+            .limit(parseInt(limit))
+
+        const totalCourse = await CourseModel.countDocuments(filter)    
+
+        res.json({
+            total:totalCourse,
+            page,
+            limit,
+            getCourse
+        })
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+courseRouter.post("/enroll",async(req,res)=>{
+    const {title,category,difficulty,description} = req.body
+    try {
+        const addCourse = new CourseModel({
+            title,
+            category,
+            difficulty,
+            description
+        })
+        await addCourse.save()
+        res.status(201).json({"message":"Successfully enrolled in the course."})
+    } catch (error) {
+        res.status(404).json({"error":"Error in post course", error})
+    }
 })
 
 module.exports = courseRouter
